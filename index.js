9b75b9eec3e6a95b3285c4d7a5f3cdf985564ac3
shivamb11 <91490404+shivamb11@users.noreply.github.com>
2023-04-20 20:05:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const form = document.querySelector('form');
 
 const search = document.querySelector('#search-bar-text');
 
+const cross = document.querySelector('.search-cross');
+
 const mic = document.querySelector('.search-mic');
 const micText = document.querySelector('#searchby-text-1');
 
@@ -22,6 +24,17 @@ grid.addEventListener('click', (e) => {
 
 body.addEventListener('click', () => {
     gridMenu.classList.add('hide');
+})
+
+search.addEventListener('input', () => {
+    if(search.value != "") {
+        cross.classList.remove('hide');
+    }
+})
+
+cross.addEventListener('click', () => {
+    search.value = "";
+    cross.classList.add('hide');
 })
 
 mic.addEventListener('mouseenter', () => {
@@ -47,6 +60,5 @@ camera.addEventListener('mouseleave', () => {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const searchData = search.value;
-    console.log(searchData);
     location.href = `https://www.google.com/search?q=${searchData}&sxsrf=APwXEdfRzt28vl-5-hc8DqFBqnsZuIw6zg%3A1681978287354&ei=r_NAZJyfFamhseMP_sWnuAQ&gs_ssp=eJzj4tLP1TfIyK1MKy5TYDRgdGDw4khLTE5Nys_PBgBmYAfL&oq=fac&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAxgBMgQIIxAnMhMILhCDARDHARCxAxDRAxCKBRBDMg0IABCKBRCxAxCDARBDMgcIABCKBRBDMgcIABCKBRBDMg0IABCKBRCxAxCDARBDMgoIABCKBRCxAxBDMggIABCABBCxAzILCAAQgAQQsQMQgwEyCggAEIoFELEDEEM6EQguEIMBEMcBELEDENEDEIAEOhEILhCABBCxAxCDARDHARDRAzoICC4QgAQQsQM6DgguEIAEELEDEIMBENQCOgoILhCKBRDUAhBDOg0ILhCKBRCxAxCDARBDOgoILhCKBRCxAxBDOg0ILhCKBRDHARDRAxBDOhMILhCKBRCxAxCDARDHARDRAxBDOhAILhCKBRCxAxCDARDUAhBDOh4ILhCDARDHARCxAxDRAxCKBRBDENwEEN4EEOAEGAFKBAhBGABQAFiVEmDfHWgAcAB4AIAB7gGIAa8HkgEFMC4zLjKYAQCgAQHAAQHaAQYIARABGBQ&sclient=gws-wiz-serp`;
 })
